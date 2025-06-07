963a60f311736b39c598870fb7f2ff8638356afb
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2025-06-07 17:25:15 +0530

    Left rotate an array by one

--- Arrays/Advanced/Fundamentals/RotateArray-1.cpp
+++ Arrays/Advanced/Fundamentals/RotateArray-1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 void rotateArray(int arr[], int n){
     int temp = arr[0];
-    for(int i=0;i<=n-1;i++){
+    for(int i=1;i<=n-1;i++){
         arr[i-1]=arr[i];
     }
     arr[n-1]=temp;
